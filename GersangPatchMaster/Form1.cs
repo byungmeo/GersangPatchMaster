b473160db968447a776b3e9fb3e3c4de0a14bc48
byungmeo <wlsrlfvkr@gmail.com>
2021-09-12 02:05:16 +0900

    multiple files download (unstable)

--- GersangPatchMaster/Form1.cs
+++ GersangPatchMaster/Form1.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
@@ -22,6 +26,11 @@ namespace GersangPatchMaster
             patchInfoDir = di.ToString();
 
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false; //버전 확인을 해야 패치 시작이 가능합니다.
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -107,64 +116,77 @@ namespace GersangPatchMaster
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //infoUrl = new Uri("http://akgersang.xdn.kinxcdn.com/Gersang/Patch/Gersang_Server/Client_info_File/" + version); //거상 패치 정보 파일이 저장된 서버 링크
-            string targetdownloadedFile = patchInfoDir + @"\" + version; //패치 정보 파일이 저장될 위치와 파일명
+            //패치 정보 파일을 다운로드 받아온다
+            string patchInfoFilePath = patchInfoDir + @"\" + version; //패치 정보 파일이 저장될 위치와 파일명
+            downloadFile(infoUrl, patchInfoFilePath); //패치 정보 파일을 PatchInfoFiles 폴더에 설치합니다.
 
-            DownloadManager downloadManager = new DownloadManager();
-            downloadManager.DownloadFile(infoUrl.ToString(), targetdownloadedFile); //패치 정보 파일을 PatchInfoFiles 폴더에 설치합니다.
+            //패치 정보 파일에서 패치 파일 리스트를 뽑아낸다
+            var files = new Dictionary<string, string>(); //key값으로 패치파일의경로, Value값으로 패치파일다운로드주소를 저장합니다.
 
+            string[] lines = File.ReadAllLines(patchInfoFilePath, Encoding.Default); //패치정보파일에서 모든 텍스트를 읽어옵니다.
 
-            //버전 확인때 유효하지 않은 버전인지 확인 하였지만, 보험으로 한번 더 체크한다. (빈 파일이라면 잘못된 버전이라는 뜻)
-            FileInfo infoFile = new FileInfo(targetdownloadedFile);
-            if (infoFile.Length == 0)
+            //패치정보파일의 첫 3줄은 쓸모없으므로 생략하고, 4번째 줄부터 읽습니다.
+            for (int i = 4; i < lines.Length; i++)
             {
-                MessageBox.Show("유효하지 않은 버전입니다. 거상 홈페이지 공지사항에 게시된 버전을 입력해주세요.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                infoFile.Delete();
-                return;
-            }
+                string[] row = lines[i].Split('\t'); //한 줄을 탭을 간격으로 나눕니다.
 
-            //패치 정보 파일에서 패치 파일 목록을 뽑아낸다.
-            List<string> patchFileList = new List<string>();
-            List<string> patchPathList = new List<string>();
-
-            string[] lines = System.IO.File.ReadAllLines(targetdownloadedFile, Encoding.Default);
-            for(int i = 4; i < lines.Length; i++)
-            {
-                string[] row = lines[i].Split('\t');
-                if(row[0] == ";EOF")
+                //만약 EOF가 등장했다면 루프를 빠져나갑니다.
+                if (row[0] == ";EOF")
                 {
                     break;
                 }
 
-                patchFileList.Add(row[1]);
-                patchPathList.Add(row[3]);
+                //
+                string patchFilePath = row[3] + row[1];
+                string patchFileUri = @"http://akgersang.xdn.kinxcdn.com/Gersang/Patch/Gersang_Server/Client_Patch_File" + patchFilePath.Replace('\\', '/');
+
+                files.Add(patchFilePath, patchFileUri);
             }
 
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < patchFileList.Count; i++)
+            //버전이름의 폴더가 없다면 생성합니다.
+            System.IO.DirectoryInfo versionDirectory = new System.IO.DirectoryInfo(Application.StartupPath + @"\" + version);
+            if (!versionDirectory.Exists) { versionDirectory.Create(); }
+
+            //버전이름의 폴더에 패치 파일을 다운로드합니다.
+            foreach (var file in files)
             {
-                sb.Append(patchFileList[i] + " " + patchPathList[i] + "\n");
-            }
-
-            MessageBox.Show(sb.ToString());
-
-            for(int i = 0; i < patchFileList.Count; i++)
-            {
-                string patchFilePath = patchPathList[i].Replace(@"\", @"/") + patchFileList[i];
-                Uri patchFileUrl = new Uri(@"http://akgersang.xdn.kinxcdn.com/Gersang/Patch/Gersang_Server/Client_Patch_File" + patchFilePath);
-
-                System.IO.DirectoryInfo versionDirectory = new System.IO.DirectoryInfo(Application.StartupPath + @"\" + version);
-                if (!versionDirectory.Exists) { versionDirectory.Create(); }
-
-                
-                //downloadManager = new DownloadManager();
-                downloadManager.DownloadFile(patchFileUrl.ToString(), versionDirectory.ToString() + @"\" + patchFileList[i]);
+                string filePath = versionDirectory.ToString() + @"\" + file.Key;
+                Uri uri = new Uri(file.Value.ToString());
+                downloadFile(uri, filePath);
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void downloadFile(Uri downloadUrl, string filePath)
         {
-            button2.Enabled = false; //버전 확인을 해야 패치 시작이 가능합니다.
+            WebClient client = new WebClient();
+
+            // 마치 사용자가 브라우저에서 작업하는 것처럼 꾸미는 코드?
+            client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+
+            string fileName = filePath.Substring(filePath.LastIndexOf('\\') + 1); //파일이름만 추출합니다
+
+            string withoutFileName = filePath.Remove(filePath.LastIndexOf('\\') + 1); //폴더경로만 추출합니다
+
+            //패치 파일은 지정된 경로에 다운됩니다. 해당 폴더가 없다면 생성합니다.
+            System.IO.DirectoryInfo fileDirectory = new System.IO.DirectoryInfo(withoutFileName);
+            if (!fileDirectory.Exists) { fileDirectory.Create(); }
+
+            //하나의 패치 파일 다운로드를 완료하면, 콘솔에 메시지를 출력합니다.
+            client.DownloadFileCompleted += (object sender, AsyncCompletedEventArgs e) =>
+            {
+                // display completion status.
+                if (e.Error != null)
+                {
+                    Console.WriteLine("다운로드 중 예기치 못한 오류가 발생하였습니다!");
+                    Console.WriteLine("오류 메시지 : " + e.Error.Message);
+                } else
+                {
+                    Console.WriteLine(fileName + " 다운로드 완료!");
+                }
+            };
+
+            //지정된 경로에 패치 파일 다운로드를 시작합니다.
+            client.DownloadFileAsync(downloadUrl, filePath);
         }
     }
 }
