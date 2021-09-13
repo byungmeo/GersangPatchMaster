c0aa7fe8a5d8cc2cae6d2a8f9fcc1605bc28ed77
byungmeo <wlsrlfvkr@gmail.com>
2021-09-14 02:04:20 +0900

    remove async keyword & optimizing code

--- GersangPatchMaster/Form1.Designer.cs
+++ GersangPatchMaster/Form1.Designer.cs
@@ -29,72 +29,129 @@ namespace GersangPatchMaster
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_checkVersion = new System.Windows.Forms.Button();
+            this.tb_version = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btn_startPatch = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_openPathFinder = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_applyPatch = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_checkVersion
             // 
-            this.button1.Location = new System.Drawing.Point(118, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "패치 버전 확인";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_checkVersion.Location = new System.Drawing.Point(167, 12);
+            this.btn_checkVersion.Name = "btn_checkVersion";
+            this.btn_checkVersion.Size = new System.Drawing.Size(99, 23);
+            this.btn_checkVersion.TabIndex = 0;
+            this.btn_checkVersion.Text = "패치 버전 확인";
+            this.btn_checkVersion.UseVisualStyleBackColor = true;
+            this.btn_checkVersion.Click += new System.EventHandler(this.btn_checkVersion_Click);
             // 
-            // textBox1
+            // tb_version
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.MaxLength = 5;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.tb_version.Location = new System.Drawing.Point(37, 12);
+            this.tb_version.MaxLength = 5;
+            this.tb_version.Name = "tb_version";
+            this.tb_version.Size = new System.Drawing.Size(100, 21);
+            this.tb_version.TabIndex = 1;
+            this.tb_version.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_version_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(223, 17);
+            this.label1.Location = new System.Drawing.Point(272, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(149, 12);
             this.label1.TabIndex = 2;
             this.label1.Text = "패치 버전을 확인해주세요.";
             // 
-            // button2
+            // btn_startPatch
             // 
-            this.button2.Location = new System.Drawing.Point(191, 140);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "패치 시작";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btn_startPatch.Location = new System.Drawing.Point(191, 204);
+            this.btn_startPatch.Name = "btn_startPatch";
+            this.btn_startPatch.Size = new System.Drawing.Size(75, 23);
+            this.btn_startPatch.TabIndex = 3;
+            this.btn_startPatch.Text = "패치 다운";
+            this.btn_startPatch.UseVisualStyleBackColor = true;
+            this.btn_startPatch.Click += new System.EventHandler(this.btn_startPatch_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(272, 145);
+            this.label2.Location = new System.Drawing.Point(272, 209);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 12);
             this.label2.TabIndex = 4;
             this.label2.Text = "선택 버전 : ";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(357, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // btn_openPathFinder
+            // 
+            this.btn_openPathFinder.Location = new System.Drawing.Point(405, 102);
+            this.btn_openPathFinder.Name = "btn_openPathFinder";
+            this.btn_openPathFinder.Size = new System.Drawing.Size(31, 23);
+            this.btn_openPathFinder.TabIndex = 6;
+            this.btn_openPathFinder.Text = "...";
+            this.btn_openPathFinder.UseVisualStyleBackColor = true;
+            this.btn_openPathFinder.Click += new System.EventHandler(this.btn_openPathFinder_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(193, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "패치를 적용할 거상 폴더 경로 지정";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(12, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "테스트 버전입니다.";
+            // 
+            // btn_applyPatch
+            // 
+            this.btn_applyPatch.Location = new System.Drawing.Point(191, 238);
+            this.btn_applyPatch.Name = "btn_applyPatch";
+            this.btn_applyPatch.Size = new System.Drawing.Size(75, 23);
+            this.btn_applyPatch.TabIndex = 9;
+            this.btn_applyPatch.Text = "패치 적용";
+            this.btn_applyPatch.UseVisualStyleBackColor = true;
+            this.btn_applyPatch.Click += new System.EventHandler(this.btn_applyPatch_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(463, 175);
+            this.ClientSize = new System.Drawing.Size(463, 273);
+            this.Controls.Add(this.btn_applyPatch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_openPathFinder);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_startPatch);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_version);
+            this.Controls.Add(this.btn_checkVersion);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GersangPatchMaster";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -103,11 +160,17 @@ namespace GersangPatchMaster
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_checkVersion;
+        private System.Windows.Forms.TextBox tb_version;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_startPatch;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_openPathFinder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btn_applyPatch;
     }
 }
 
--- GersangPatchMaster/Form1.cs
+++ GersangPatchMaster/Form1.cs
@@ -5,26 +5,20 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
-using System.Net.Http;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
-namespace GersangPatchMaster
-{   
-    public partial class Form1 : Form
-    {
-        string patchInfoDir;
-        Uri infoUrl;
-        string version;
-        int patchFileCount;
-        int downloadCompletedCount;
-        Stopwatch sw;
+namespace GersangPatchMaster {
+    public partial class Form1 : Form {
+        private string patchInfoDir;
+        private Uri infoUrl;
+        private string version;
+        private int patchFileCount;
+        private int downloadCompletedCount;
+        private Stopwatch sw;
 
-        public Form1()
-        {
+        public Form1() {
             //프로그램 실행시 Data 폴더 확인 및 없을경우 Data 폴더 생성
             System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(Application.StartupPath + @"\PatchInfoFiles");
             if (!di.Exists) { di.Create(); }
@@ -33,32 +27,28 @@ namespace GersangPatchMaster
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            button2.Enabled = false; //버전 확인을 해야 패치 시작이 가능합니다.
+        private void Form1_Load(object sender, EventArgs e) {
+            btn_startPatch.Enabled = false; //버전 확인을 해야 패치 시작이 가능합니다.
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            version = textBox1.Text;
+        //패치 버전 확인 버튼
+        private void btn_checkVersion_Click(object sender, EventArgs e) {
+            version = tb_version.Text;
 
             //패치버전을 입력하지 않았다면 메시지를 출력합니다.
-            if(string.IsNullOrEmpty(version))
-            {
+            if(string.IsNullOrEmpty(version)) {
                 MessageBox.Show("패치버전을 입력해주세요.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 return;
             }
 
             //패치버전은 반드시 숫자로 5자 입니다.
-            if(version.Length != 5)
-            {
+            if(version.Length != 5) {
                 MessageBox.Show("올바른 버전을 입력해주세요. (5자리가 아닙니다!)\n예시) 30417", "오류", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 return;
             }
 
             //해당 패치 버전이 언제 거상 패치 서버에 게시된 것인지 확인합니다.
-            try
-            {
+            try {
                 //코드 출처 : https://docs.microsoft.com/ko-kr/dotnet/api/system.exception?view=net-5.0
 
                 Uri myUri = new Uri(@"http://akgersang.xdn.kinxcdn.com/Gersang/Patch/Gersang_Server/Client_info_File/" + version);
@@ -69,15 +59,11 @@ namespace GersangPatchMaster
                 DateTime today = DateTime.Now;
 
                 // Uses the LastModified property to compare with today's date.
-                if (DateTime.Compare(today, myHttpWebResponse.LastModified) == 0)
-                {
+                if (DateTime.Compare(today, myHttpWebResponse.LastModified) == 0) {
                     Console.WriteLine("\nThe requested URI entity was modified today");
                     label1.Text = "오늘 게시된 업데이트입니다.";
-                }
-                else
-                {
-                    if (DateTime.Compare(today, myHttpWebResponse.LastModified) == 1)
-                    {
+                } else {
+                    if (DateTime.Compare(today, myHttpWebResponse.LastModified) == 1) {
                         Console.WriteLine("\nThe requested URI was last modified on:{0}",
                                             myHttpWebResponse.LastModified);
                         label1.Text = myHttpWebResponse.LastModified.ToString() + "\n에 게시된 업데이트입니다.";
@@ -87,49 +73,43 @@ namespace GersangPatchMaster
                 infoUrl = myUri;
                 // Releases the resources of the response.
                 myHttpWebResponse.Close();
-            } catch(System.Net.WebException exception)
-            {
+            } catch(System.Net.WebException exception) {
                 MessageBox.Show("유효하지 않은 버전입니다. 거상 홈페이지 공지사항에 게시된 버전을 입력해주세요.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);  
                 Console.WriteLine(exception);
-                button2.Enabled = false;
+                btn_startPatch.Enabled = false;
                 label2.Text = "선택 버전 : ";
                 label1.Text = "패치 버전을 확인해주세요.";
                 return;
             }
 
-            button2.Enabled = true;
+            btn_startPatch.Enabled = true;
             label2.Text = "선택 버전 : v" + version;
         }
 
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
+        private void tb_version_KeyPress(object sender, KeyPressEventArgs e) {
             //코드 출처 : https://stackoverflow.com/questions/463299/how-do-i-make-a-textbox-that-only-accepts-numbers
 
             //버전 명은 숫자 외에 입력이 되지 않도록 합니다.
 
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
-            {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.')) {
                 e.Handled = true;
             }
 
             // only allow one decimal point
-            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
-            {
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1)) {
                 e.Handled = true;
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
+        //패치 시작 확인 버튼
+        private void btn_startPatch_Click(object sender, EventArgs e) {
             patchFileCount = 0;
             downloadCompletedCount = 0;
 
-            //패치 정보 파일을 다운로드 받아온다
+            //패치 정보 파일을 다운로드 한다.
             string patchInfoFilePath = patchInfoDir + @"\" + version; //패치 정보 파일이 저장될 위치와 파일명
-
             WebClient infoDownloader = new WebClient();
             infoDownloader.DownloadFile(infoUrl, patchInfoFilePath); //패치 정보 파일이 완료되기 전에 패치 정보 파일에 접근해버려서 동기식으로 다운
-            //downloadFile(infoUrl, patchInfoFilePath); //패치 정보 파일을 PatchInfoFiles 폴더에 설치합니다.
 
             //패치 정보 파일에서 패치 파일 리스트를 뽑아낸다
             Dictionary<string, string> files = new Dictionary<string, string>(); //key값으로 패치파일의경로, Value값으로 패치파일다운로드주소를 저장합니다.
@@ -139,13 +119,11 @@ namespace GersangPatchMaster
             Console.WriteLine("다운받아야하는 패치 파일의 갯수 : " + patchFileCount);
 
             //패치정보파일의 첫 4줄은 쓸모없으므로 생략하고, 5번째 줄부터 읽습니다.
-            for (int i = 4; i < lines.Length; i++)
-            {
+            for (int i = 4; i < lines.Length; i++) {
                 string[] row = lines[i].Split('\t'); //한 줄을 탭을 간격으로 나눕니다.
 
                 //만약 EOF가 등장했다면 루프를 빠져나갑니다.
-                if (row[0] == ";EOF")
-                {
+                if (row[0] == ";EOF") {
                     break;
                 }
 
@@ -162,80 +140,92 @@ namespace GersangPatchMaster
 
             sw = new Stopwatch();
             sw.Start();
-            //버전이름의 폴더에 패치 파일을 다운로드합니다.
-            foreach (var file in files)
-            {
-                string filePath = versionDirectory.ToString() + @"\" + file.Key;
+            //버전이름의 폴더에 패치 파일을 다운로드합니다. (버전이름_unpack 폴더에는 압축해제된 패치 파일)
+            foreach (var file in files) {
+                string filePath = versionDirectory.ToString() + file.Key;
                 Uri uri = new Uri(file.Value.ToString());
                 downloadFile(uri, filePath);
             }
         }
 
-        private async void downloadFile(Uri downloadUrl, string filePath)
-        {
-            WebClient client = new WebClient();
+        private void btn_openPathFinder_Click(object sender, EventArgs e) {
+            folderBrowserDialog1.SelectedPath = null;
+            folderBrowserDialog1.ShowDialog();
 
-            // 마치 사용자가 브라우저에서 작업하는 것처럼 꾸미는 코드?
-            client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+            if ((folderBrowserDialog1.SelectedPath.Length) != 0) {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
 
+        private void btn_applyPatch_Click(object sender, EventArgs e) {
+            DirectoryInfo dirPath = new System.IO.DirectoryInfo(Application.StartupPath + @"\" + version + "_unpack");
+
+            //압축해제한 패치 파일을 지정한 경로의 본클라에 적용시킵니다.
+            applyPatchFileInDirectory(dirPath);
+        }
+
+        private void applyPatchFileInDirectory(DirectoryInfo directoryPath) {
+            foreach (DirectoryInfo dirInfo in directoryPath.GetDirectories()) {
+                applyPatchFileInDirectory(dirInfo);
+            }
+
+            foreach (FileInfo fileInfo in directoryPath.GetFiles()) {
+                File.Copy(fileInfo.FullName, textBox2.Text + @"\" + fileInfo.Name, true);
+            }
+        }
+
+        //파일 다운로드 코드 (
+        private void downloadFile(Uri downloadUrl, string filePath) {
             string fileName = filePath.Substring(filePath.LastIndexOf('\\') + 1); //파일이름만 추출합니다
 
-            string withoutFileName = filePath.Remove(filePath.LastIndexOf('\\') + 1); //폴더경로만 추출합니다
-
-            //패치 파일은 지정된 경로에 다운됩니다. 해당 폴더가 없다면 생성합니다.
-            System.IO.DirectoryInfo fileDirectory = new System.IO.DirectoryInfo(withoutFileName);
-            if (!fileDirectory.Exists) { fileDirectory.Create(); }
-
-            //하나의 패치 파일 다운로드를 완료하면, 콘솔에 메시지를 출력합니다.
-            //그리고 압축해제를 시작합니다.
-            client.DownloadFileCompleted += (object sender, AsyncCompletedEventArgs e) =>
-            {
-                // display completion status.
-                if (e.Error != null)
-                {
-                    Console.WriteLine("다운로드 중 예기치 못한 오류가 발생하였습니다!");
-                    Console.WriteLine("오류 메시지 : " + e.Error.Message);
-                } else
-                {
-                    Console.WriteLine(fileName + " 다운로드 완료! " + (patchFileCount - (++downloadCompletedCount)) + "개 남음!");
-                    if(downloadCompletedCount == patchFileCount)
-                    {
-                        Console.WriteLine("모든 패치파일 다운로드 완료!");
-                        sw.Stop();
-                        Console.WriteLine("다운로드 완료까지 " + sw.ElapsedMilliseconds.ToString() + "ms초 경과");
-
-                        Console.WriteLine("압축 해제를 시작합니다!");
-                        extractGszInDirectory(new System.IO.DirectoryInfo(Application.StartupPath + @"\" + version));
-                    }
+            //이미 패치 파일을 다운로드한 경우, 다운받지 않습니다.
+            //Console.WriteLine("Exist : " + filePath.Remove(filePath.Length - 4));
+            if (File.Exists(filePath.Remove(filePath.Length - 4))) {
+                Console.WriteLine(fileName + "는 이미 존재합니다! " + (patchFileCount - (++downloadCompletedCount)) + "개 남음!");
+                if (downloadCompletedCount == patchFileCount) {
+                    Console.WriteLine("모든 패치파일 다운로드 및 압축해제 완료!");
+                    sw.Stop();
+                    Console.WriteLine("다운로드 완료까지 " + sw.ElapsedMilliseconds.ToString() + "ms초 경과");
                 }
-            };
-
-            //지정된 경로에 패치 파일 다운로드를 시작합니다.
-            client.DownloadFileAsync(downloadUrl, filePath);
-        }
-
-        //한 폴더 내에 있는 모든 Gsz 파일을 압축해제 합니다. (폴더 안의 폴더까지)
-        private void extractGszInDirectory(DirectoryInfo directoryPath)
-        {
-            foreach (DirectoryInfo dirInfo in directoryPath.GetDirectories())
-            {
-                extractGszInDirectory(dirInfo);
+                return;
             }
 
-            foreach (FileInfo fileInfo in directoryPath.GetFiles("*.gsz"))
-            {
-                string dirPath = directoryPath.FullName;
-                dirPath = dirPath.Remove(0, Application.StartupPath.Length);
-                dirPath = dirPath.Insert(6, "_unpack");
+            using (WebClient client = new WebClient()) {
+                // 마치 사용자가 브라우저에서 작업하는 것처럼 꾸미는 코드?
+                client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
 
-                extractGsz(fileInfo.FullName, Application.StartupPath + dirPath);
+                //패치 파일은 지정된 경로에 다운됩니다. 해당 폴더가 없다면 생성합니다.
+                DirectoryInfo fileDirectory = new DirectoryInfo(new FileInfo(filePath).DirectoryName);
+                if (!fileDirectory.Exists) { fileDirectory.Create(); }
+
+
+                //하나의 패치 파일 다운로드를 완료하면, 압축해제 후 압축파일을 삭제하는 콜백메서드를 연결합니다.
+                client.DownloadFileCompleted += (object sender, AsyncCompletedEventArgs e) => {
+                    // display completion status.
+                    if (e.Error != null) {
+                        Console.WriteLine("다운로드 중 예기치 못한 오류가 발생하였습니다!");
+                        Console.WriteLine("오류 메시지 : " + e.Error.Message);
+                    } else {
+                        Console.WriteLine(fileName + " 다운로드 완료! " + (patchFileCount - (++downloadCompletedCount)) + "개 남음!");
+                        try {
+                            ZipFile.ExtractToDirectory(filePath, new FileInfo(filePath).DirectoryName);
+                            File.Delete(filePath);
+                            Console.WriteLine(fileName + " 압축해제 및 압축파일 삭제 완료!");
+                        } catch(Exception ex) {
+                            Console.WriteLine("downloadFileCompleted : " + ex.Message);
+                        }
+
+                        if (downloadCompletedCount == patchFileCount) {
+                            Console.WriteLine("모든 패치파일 다운로드 및 압축해제 완료!");
+                            sw.Stop();
+                            Console.WriteLine("다운로드 완료까지 " + sw.ElapsedMilliseconds.ToString() + "ms초 경과");
+                        }
+                    }
+                };
+
+                //지정된 경로에 패치 파일 다운로드를 시작합니다.
+                client.DownloadFileAsync(downloadUrl, filePath);
             }
-        }
-
-        //모든 gsz 파일을 압축해제 합니다.
-        private async void extractGsz(string zipPath, string extractPath)
-        {
-            ZipFile.ExtractToDirectory(zipPath, extractPath);
         }
     }
 }
